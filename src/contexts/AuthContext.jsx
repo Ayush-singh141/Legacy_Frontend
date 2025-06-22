f23efc49f6249ce4295bdd5ca03abdb32537a506
Ayush Singh <ayushsinghgaur2100@gmail.com>
2025-06-22 15:22:26 +0530

    new changes redone

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,7 +7,7 @@ const AuthContext = createContext();
 export function useAuth() {
   return useContext(AuthContext);
 }
-
+//comment
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem('token'));
